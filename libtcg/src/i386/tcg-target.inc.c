0567f088482ed1e6aa2f8d648d5613d0622b88de
codefiring <hhx970127@163.com>
2022-01-18 02:10:41 -0800

    libtcg: disable TLB

--- libtcg/src/i386/tcg-target.inc.c
+++ libtcg/src/i386/tcg-target.inc.c
@@ -1522,7 +1522,7 @@ static inline void tcg_out_tlb_load(TCGContext *s, TCGReg addrlo, TCGReg addrhi,
     /* jne slow_path */
     // tcg_out_opc(s, OPC_JCC_long + JCC_JNE, 0, 0, 0);
     
-    // In order to monitor memory we disable tlb to force qemu use softmmu to translate address. by codefiring1024@163.com
+    // In order to monitor memory we disable tlb to force qemu use softmmu to translate address. By codefiring1024@163.com
     tcg_out_opc(s, OPC_JMP_long, 0, 0, 0); 
     label_ptr[0] = s->code_ptr;
     s->code_ptr += 4;
